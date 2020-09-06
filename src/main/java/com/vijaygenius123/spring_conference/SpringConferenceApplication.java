38841c39e5d3f02330a35d0d5eb7a4165a1c7c48
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-09-06 20:32:25 +0530

    Initial commit

---
+++ src/main/java/com/vijaygenius123/spring_conference/SpringConferenceApplication.java
@@ -0,0 +1,13 @@
+package com.vijaygenius123.spring_conference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConferenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringConferenceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vijaygenius123/spring_conference/SpringConferenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vijaygenius123.spring_conference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConferenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
